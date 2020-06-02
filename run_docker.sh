1a56bfefe69001b76af14da449832b34960e8257
EC2 Default User <ec2-user@ip-172-31-16-81.us-west-2.compute.internal>
2020-06-02 18:48:18 +0000

    InitialCommit

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build . -t talkdeepak/udacity_microservice:v1
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run -p 127.0.0.1:8000:80 talkdeepak/udacity_microservice:v1
